13e5fbe9829d3533fdf6e6ec34dbf1845c6b9dd0
Joao Fidelis <joaogfidelis@gmail.com>
2019-08-06 23:21:58 -0300

    product query and basic product resolver

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,9 +1,33 @@
 import { IResolvers } from "graphql-tools";
+const fetch = require("node-fetch");
+interface ProductArg {
+  slug: string;
+}
+
+const queries = {
+  helloWorld(_: void, args: void): string {
+    return "Hello world!";
+  },
+  product: async (_: any, args: ProductArg) => {
+    const { slug } = args;
+    const fetchData = await fetch(
+      `http://boticario.vtexcommercestable.com.br/api/catalog_system/pub/products/search/${slug}/p`
+    );
+    const results = await fetchData.json();
+    return results[0];
+  }
+};
+
+const productResolvers = {
+  Product: {
+    teste: () => "hello"
+  }
+};
+
 const resolverMap: IResolvers = {
+  ...productResolvers,
   Query: {
-    helloWorld(_: void, args: void): string {
-      return "Hello world!";
-    }
+    ...queries
   }
 };
 export default resolverMap;
